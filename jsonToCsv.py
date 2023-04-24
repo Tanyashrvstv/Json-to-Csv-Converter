fd8e5565e956dc9497e307926abc0828befce840
Tanya Shrivastava <73074722+Tanyashrvstv@users.noreply.github.com>
2023-04-24 12:16:58 +0530

    Add files via upload

---
+++ jsonToCsv.py
@@ -0,0 +1,16 @@
+import json
+
+if __name__ == '__main__':
+    try:
+        with open('inputFile.json','r') as f:
+            data = json.loads(f.read())
+            
+        output = ','.join(*[data[0]])
+        
+        for obj in data:
+            output += f'\n{obj["Name"]},{obj["age"]},{obj["birthyear"]}'
+        
+        with open('outputFile.csv', 'w') as f:
+            f.write(output)
+    except Exception as ex:
+            print(f'Error: something')
